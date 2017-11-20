f6ff6035c1e3efc81d2b265d2dc76bcbdc0ef171
Paul Klinger <klinger.paul@gmail.com>
2017-11-20 23:54:58 +0100

    code cleanup, use a bit more abstraction

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,83 @@ extern crate regex;
 use std::process::{Command,Stdio};
 use std::io::{BufRead, BufReader};
 use std::str;
+use std::ops::{Index, IndexMut};
 
 use regex::Regex;
 
 const SIZE: usize = 19;
 const ROW_INDICES: &'static str = "ABCDEFGHJKLMNOPQRST";
 
-fn get_index((row, col): (usize, usize)) -> usize {
-    return (row - 1) * SIZE + (col - 1)
+#[derive(PartialEq, Clone, Copy)]
+struct Coord(usize, usize);
+
+#[derive(Clone, Copy)]
+enum Player {
+    White,
+    Black,
 }
 
-fn update_board(board: &mut Vec<char>, row_str: &str, col_str: &str, current_player: char) {
+impl Player {
+    fn symbol(&self) -> char {
+        match *self {
+            Player::White => 'o',
+            Player::Black => 'x',
+        }
+    }
+}
+
+struct Board {
+    board: Vec<char>,
+    size: usize
+}
+
+impl Index<Coord> for Board {
+    type Output = char;
+
+    fn index(&self, coord: Coord) -> &char {
+        &self.board[get_index(self.size, coord)]
+    }
+}
+
+impl IndexMut<Coord> for Board {
+    fn index_mut(&mut self, coord: Coord) -> &mut char {
+        &mut self.board[get_index(self.size, coord)]
+    }
+}
+
+impl Board {
+    fn new(size: usize) -> Board {
+        Board {
+            board: vec!['.'; size * size],
+            size: size,
+        }
+    }
+}
+
+fn get_index(size: usize, Coord(row, col): Coord) -> usize {
+    return (row - 1) * size + (col - 1)
+}
+
+fn update_board(board: &mut Board, row_str: &str, col_str: &str, current_player: Player) {
     let row_index = ROW_INDICES.find(row_str).unwrap() + 1;
     let col_index: usize = col_str.parse().unwrap();
-    board[get_index((row_index, col_index))] = current_player;
+    board[Coord(row_index, col_index)] = current_player.symbol();
 
     resolve_capture(board);
 }
 
-fn resolve_capture(board: &mut Vec<char>) {
-    let mut processed_coords: Vec<(usize, usize)> = Vec::new();
-    let mut chain_coords: Vec<(usize, usize)> = Vec::new();
-    let mut liberties: Vec<(usize, usize)> = Vec::new();
-    for row in 1..SIZE+1 {
-        for col in 1..SIZE+1 {
-            if board[get_index((row, col))] != '.' && ! processed_coords.contains(&(row,col)) {
+fn resolve_capture(board: &mut Board) {
+    let mut processed_coords: Vec<Coord> = Vec::new();
+    let mut chain_coords: Vec<Coord> = Vec::new();
+    let mut liberties: Vec<Coord> = Vec::new();
+    for row in 1..board.size+1 {
+        for col in 1..board.size+1 {
+            if board[Coord(row, col)] != '.' && ! processed_coords.contains(&Coord(row,col)) {
                 process_chain(board, row, col, &mut chain_coords, &mut liberties);
                 if liberties.len() == 0 {
                     println!("{} stones captured!", chain_coords.len());
-                    for &(c_row, c_col) in &chain_coords {
-                        board[get_index((c_row, c_col))] = '.';
+                    for &Coord(c_row, c_col) in &chain_coords {
+                        board[Coord(c_row, c_col)] = '.';
                     }
                 }
                 processed_coords.append(&mut chain_coords); // this clears chain_coords
@@ -42,19 +89,19 @@ fn resolve_capture(board: &mut Vec<char>) {
     }
 }
 
-fn process_chain(board: &Vec<char>, row: usize, col: usize, chain_coords: &mut Vec<(usize, usize)>, liberties: &mut Vec<(usize, usize)>) {
-    if row < 1 || col < 1 || row > SIZE || col > SIZE {
+fn process_chain(board: &Board, row: usize, col: usize, chain_coords: &mut Vec<Coord>, liberties: &mut Vec<Coord>) {
+    if row < 1 || col < 1 || row > board.size || col > board.size {
         return;
     }
-    let val = board[get_index((row,col))];
-    let coord = (row, col);
+    let val = board[Coord(row,col)];
+    let coord = Coord(row, col);
     if val == '.' {
         liberties.push(coord);
         return;
     }
     else if ! chain_coords.contains(&coord) 
             && (chain_coords.len() == 0
-                || val == board[get_index(*chain_coords.get(0).unwrap())]
+                || val == board[*chain_coords.get(0).unwrap()]
                 )
     {
         chain_coords.push(coord);
@@ -66,18 +113,17 @@ fn process_chain(board: &Vec<char>, row: usize, col: usize, chain_coords: &mut V
     
 }
 
-fn print_board(board: &Vec<char>) {
-    for row in 1..SIZE+1 {
+fn print_board(board: &Board) {
+    for row in 1..board.size+1 {
         let mut out_line = String::new();
-        for col in 1..SIZE+1 {
-            out_line.push(board[get_index((row, col))]);
+        for col in 1..board.size+1 {
+            out_line.push(board[Coord(row, col)]);
         }
         println!("{}",out_line);
     }
 }
 
 fn main() {
-    //println!("Hello, world!");
     let mut child = Command::new("./autogtp.exe")
         .arg("-k")
         .arg("sgfs")
@@ -89,8 +135,8 @@ fn main() {
     let mut buffer: Vec<u8> = Vec::new();
     let mut line: String;
 
-    let mut board: Vec<char> = vec!['.'; SIZE * SIZE];
-    let mut current_player = 'o';
+    let mut board: Board = Board::new(SIZE);
+    let mut current_player = Player::White;
     let move_regex = Regex::new(r"[ \n]\d+ \(([A-Z])(\d+)\)").unwrap();
     let end_regex = Regex::new(r"Game has ended").unwrap();
     assert!(move_regex.is_match(" 245 (F18)"));
@@ -101,8 +147,8 @@ fn main() {
         buffer.clear();
 
         if end_regex.is_match(&line) {
-            board = vec!['.'; SIZE * SIZE];
-            current_player = 'o';
+            board = Board::new(SIZE);
+            current_player = Player::White;
         }
 
         println!("{}",line);
@@ -117,7 +163,9 @@ fn main() {
             _ => {},
         }
 
-        if current_player == 'o' {current_player = 'x'}
-        else {current_player = 'o'}
+        current_player = match current_player {
+            Player::White => Player::Black,
+            Player::Black => Player::White,
+        }
     }
 }
