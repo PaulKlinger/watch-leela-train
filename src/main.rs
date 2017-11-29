f63605ebebb8e77cab1486f2439aaaa4c443f7c5
Paul Klinger <klinger.paul@gmail.com>
2017-11-30 00:01:34 +0100

    formatted code using rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![feature(inclusive_range_syntax)]
 extern crate regex;
 
-use std::process::{Command,Stdio};
+use std::process::{Command, Stdio};
 use std::io::{BufRead, BufReader};
 use std::str;
 use std::ops::{Index, IndexMut};
@@ -33,7 +33,7 @@ impl Player {
 
 struct Board {
     board: Vec<char>,
-    size: usize
+    size: usize,
 }
 
 impl Index<Coord> for Board {
@@ -72,10 +72,15 @@ impl Board {
 }
 
 fn get_index(size: usize, Coord(row, col): Coord) -> usize {
-    return (row - 1) * size + (col - 1)
+    return (row - 1) * size + (col - 1);
 }
 
-fn update_board(board: &mut Board, row_str: &str, col_str: &str, current_player: Player) -> Vec<u8> {
+fn update_board(
+    board: &mut Board,
+    row_str: &str,
+    col_str: &str,
+    current_player: Player,
+) -> Vec<u8> {
     let row_index = ROW_INDICES.find(row_str).unwrap() + 1;
     let col_index: usize = col_str.parse().unwrap();
     board[Coord(row_index, col_index)] = current_player.symbol();
@@ -83,7 +88,7 @@ fn update_board(board: &mut Board, row_str: &str, col_str: &str, current_player:
     resolve_capture(board)
 }
 
-fn resolve_capture(board: &mut Board) -> Vec<u8>{
+fn resolve_capture(board: &mut Board) -> Vec<u8> {
     let mut out = Vec::new();
     let mut processed_coords: HashSet<Coord> = HashSet::new();
     let mut chain_coords: HashSet<Coord> = HashSet::new();
@@ -91,7 +96,7 @@ fn resolve_capture(board: &mut Board) -> Vec<u8>{
     for row in 1..=board.size {
         for col in 1..=board.size {
             let coord = Coord(row, col);
-            if board[coord] != '.' && ! processed_coords.contains(&coord) {
+            if board[coord] != '.' && !processed_coords.contains(&coord) {
                 process_chain(board, coord, &mut chain_coords, &mut liberties);
                 if liberties.len() == 0 {
                     out.push(chain_coords.len() as u8);
@@ -107,28 +112,30 @@ fn resolve_capture(board: &mut Board) -> Vec<u8>{
     out
 }
 
-fn process_chain(board: &Board, coord: Coord, chain_coords: &mut HashSet<Coord>, liberties: &mut HashSet<Coord>) {
+fn process_chain(
+    board: &Board,
+    coord: Coord,
+    chain_coords: &mut HashSet<Coord>,
+    liberties: &mut HashSet<Coord>,
+) {
     let Coord(row, col) = coord;
     if row < 1 || col < 1 || row > board.size || col > board.size {
         return;
     }
-    
+
     let val = board[coord];
     if val == '.' {
         liberties.insert(coord);
         return;
-    }
-    else if ! chain_coords.contains(&coord) 
-            && (chain_coords.len() == 0
-                || val == board[*chain_coords.iter().next().unwrap()]
-                ){
+    } else if !chain_coords.contains(&coord)
+        && (chain_coords.len() == 0 || val == board[*chain_coords.iter().next().unwrap()])
+    {
         chain_coords.insert(coord);
         process_chain(board, Coord(row - 1, col), chain_coords, liberties);
         process_chain(board, Coord(row + 1, col), chain_coords, liberties);
         process_chain(board, Coord(row, col - 1), chain_coords, liberties);
         process_chain(board, Coord(row, col + 1), chain_coords, liberties);
     }
-    
 }
 
 fn get_autogtp_version() -> String {
@@ -136,8 +143,12 @@ fn get_autogtp_version() -> String {
         .arg("--version")
         .output()
         .expect("failed to start autogtp");
-    String::from_utf8_lossy(&output.stdout).trim_right().split(' ').last()
-        .expect("Failed to determine autogtp version!").into()
+    String::from_utf8_lossy(&output.stdout)
+        .trim_right()
+        .split(' ')
+        .last()
+        .expect("Failed to determine autogtp version!")
+        .into()
 }
 
 fn main() {
@@ -156,7 +167,7 @@ fn main() {
 
     let mut child_out = BufReader::new(child.stdout.as_mut().unwrap());
     let mut child_err = BufReader::new(child.stderr.as_mut().unwrap());
-    
+
     let mut buffer: Vec<u8> = Vec::new();
     let mut line: String;
 
@@ -169,7 +180,7 @@ fn main() {
 
     loop {
         // Early autogtp versions printed to stderr instead of stdout
-        if vec!["v1", "v2", "v3", "v4"].contains(&&autogtp_version[..]) { 
+        if vec!["v1", "v2", "v3", "v4"].contains(&&autogtp_version[..]) {
             child_err.read_until(')' as u8, &mut buffer).unwrap();
         } else {
             child_out.read_until(')' as u8, &mut buffer).unwrap();
@@ -190,17 +201,22 @@ fn main() {
                     &mut board,
                     caps.get(1).unwrap().as_str(),
                     caps.get(2).unwrap().as_str(),
-                    current_player);
+                    current_player,
+                );
                 if !captures.is_empty() {
-                    out.push_str(&format!(" Captured {} stones.",
-                        captures.iter().map(|c| c.to_string())
-                            .collect::<Vec<String>>().join(", ")
-                        ))
+                    out.push_str(&format!(
+                        " Captured {} stones.",
+                        captures
+                            .iter()
+                            .map(|c| c.to_string())
+                            .collect::<Vec<String>>()
+                            .join(", ")
+                    ))
                 }
-            },
-            _ => {},
+            }
+            _ => {}
         }
-        
+
         if move_or_pass_regex.is_match(&line) {
             out.push_str(&board.to_string());
 
